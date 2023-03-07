574a700d1c8f5da71cdfd2710209db0cdd83e203
Anastasiya Laboda <anastasiya.laboda@gmail.com>
2023-03-07 12:57:41 +0300

    Homework 1

--- src/main/java/Homework1/Calculator.java
+++ src/main/java/Homework1/Calculator.java
@@ -1,2 +1,55 @@
-package Homework1;public class Calculator {
+package Homework1;
+
+import java.util.Scanner;
+
+public class Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        calculation(getNumber(), getOperator(), getNumber());
+    }
+
+    public static double getNumber() {
+        System.out.println("Enter number");
+        double number;
+        if (scanner.hasNextDouble()) {
+            number = scanner.nextDouble();
+        } else {
+            System.out.println("Something is wrong. Try again");
+            scanner.next();
+            number = getNumber();
+        }
+        return number;
+    }
+
+    public static char getOperator() {
+        System.out.println("Enter operator (+, -, *, /)");
+        char operator;
+        if (scanner.hasNext()) {
+            operator = scanner.next().charAt(0);
+        } else {
+            System.out.println("Enter the correct operator and try again");
+            operator = getOperator();
+        }
+        return operator;
+    }
+
+    public static void calculation(double number1, char operator, double number2) {
+        switch (operator) {
+            case '+':
+                System.out.println("Result: " + (number1 + number2) + "\n");
+                break;
+            case '-':
+                System.out.println("Result: " + (number1 - number2) + "\n");
+                break;
+            case '*':
+                System.out.println("Result: " + (number1 * number2) + "\n");
+                break;
+            case '/':
+                System.out.println("Result: " + (number1 / number2) + "\n");
+                break;
+            default:
+                System.out.println("Error! Enter correct operator\n");
+        }
+    }
 }
